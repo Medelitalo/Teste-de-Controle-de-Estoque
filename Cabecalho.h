e99122c1acb93958b3aa8736c04efc45d053d4d6
Medelitalo <126923575+Medelitalo@users.noreply.github.com>
2023-06-01 11:37:25 -0400

    Update Cabecalho.h

--- Cabecalho.h
+++ Cabecalho.h
@@ -3,7 +3,7 @@
 //Procurar Estoque no Banco de dados. 
 void ProcEstoq(); 
 
-//Adiciona Marca e modole de pneu no banco de dados. 
+//Adiciona Marca e modelo de pneu no banco de dados. 
 void AddPneu(); 
 
 //mostra todo o estoque 
